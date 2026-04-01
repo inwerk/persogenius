3956a3e175fd8ae9156d02cf75259226204413cb
inwerk <142039445+inwerk@users.noreply.github.com>
2026-04-02 01:17:25 +0200

    refactor: use middleware to inject CSP nonce and headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,3 @@
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`
-  .replace(/\n/g, "")
-  .trim()
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   async headers() {
@@ -20,10 +5,6 @@ const nextConfig = {
       {
         source: "/(.*)",
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value: cspHeader,
-          },
           {
             key: "Strict-Transport-Security",
             value: "max-age=31536000; includeSubDomains",
